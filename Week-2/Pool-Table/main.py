ea59ec9541af3344e7f9764fa24f0b52cfc73a6f
snmsaj <cehd@CEHDs-MacBook-Pro.local>
2021-02-12 16:05:40 -0500

    Updated pool_table and main.py

--- Week-2/Pool-Table/main.py
+++ Week-2/Pool-Table/main.py
@@ -1,65 +1,54 @@
-from pool_table import PoolTable
-
-def menu():
-    print("\nPress 1 to check-in available tables")
-    print("Press 2 to check-out/close table")
-    print("Press 3 to list all tables and start times")
-    print("Press q to quit\n")
+from pool_table import PoolTable, menu, write_to_file
+import json
+from datetime import datetime
 
 
 tables = []
+checkout_tables = []
 
 for i in range(1,13): 
     table = PoolTable(i)
     tables.append(table)
 
+
 choice = ""
 
 while choice != "q":
-    menu()
-    choice = input("Enter the option: ")
+    try:
+        menu()
+        choice = input("Enter the option: ")
 
-    if choice == "1":
-        for i in  range(0,len(tables)):
-            if tables[i].occupied == False:
-                print(f"Table {tables[i].table_num}")
+        if choice == "1":
+            for i in  range(0,len(tables)):
+                if tables[i].occupied == False:
+                    print(f"Table {tables[i].table_num}")
 
-        while True:
-            choose_table = int(input("Enter table # to check in: "))
-            
-            if tables[choose_table-1].occupied == True:
-                print(f"Pool Table {tables[choose_table-1].table_num} is currently occupied.")
-            else:
-                tables[choose_table-1].check_in()
-                break
+            while True:
+                choose_table = int(input("Enter table # to check in: "))
+                
+                if tables[choose_table-1].occupied == True:
+                    print(f"Pool Table {tables[choose_table-1].table_num} is currently occupied.")
+                else:
+                    tables[choose_table-1].check_in()
+                    break
 
-    elif choice == "2":
-        for i in  range(0,len(tables)):
-            if tables[i].occupied == True:
-                print(f"Table {tables[i].table_num}")
+        elif choice == "2":
+            for i in  range(0,len(tables)):
+                if tables[i].occupied == True:
+                    print(f"Table {tables[i].table_num}")
 
-        choose_table = int(input("Enter table # to check out: "))
-        tables[choose_table-1].check_out()
+            choose_table = int(input("Enter table # to check out: "))
+            tables[choose_table-1].check_out()
+            checkout_tables.append(tables[choose_table-1])
+            write_to_file(checkout_tables)
 
-    elif choice == "3":
-        for i in  range(0,len(tables)):
-            if tables[i].occupied == False:
-                print(f"Table {tables[i].table_num}: NOT OCCUPIED")
-            else:
-                print(f"Table {tables[i].table_num}: OCCUPIED - {tables[i].start_time.time().strftime('%H:%M')}")
+        elif choice == "3":
+            for i in  range(0,len(tables)):
+                if tables[i].occupied == False:
+                    print(f"Table {tables[i].table_num}: NOT OCCUPIED")
+                else:
+                    print(f"Table {tables[i].table_num}: OCCUPIED - {tables[i].start_time.time().strftime('%H:%M')}")
 
-    
-
-
-
-
-# datetime.now().strftime('%m-%d-%Y')
-
-# print(tables[1].table_num)
-# tables[1].check_in()
-# print(tables[1].occupied)
-# tables[1].check_out()
-# print(tables[1].occupied)
-# print(tables[1].start_time)
-# print(tables[1].end_time)
-# print(tables[1].time_played())
+    except (ValueError, IndexError):
+        print("Error! Invalid input.")
+        continue
--- Week-2/Pool-Table/pool_table.py
+++ Week-2/Pool-Table/pool_table.py
@@ -19,8 +19,17 @@ class PoolTable:
         return self.end_time - self.start_time
 
 
+def menu():
+    print("\nPress 1 to check-in available tables")
+    print("Press 2 to check-out/close table")
+    print("Press 3 to list all tables and start times")
+    print("Press q to quit\n")
 
 
-
-
-
+def write_to_file(add_checkout_table):
+    with open(f"{datetime.now().strftime('%m-%d-%Y')}.txt","w") as file:
+        for i in range(0,len(add_checkout_table)):
+            file.write(f"Table Number: {add_checkout_table[i].table_num}\n")
+            file.write(f"Start Time: {add_checkout_table[i].start_time}\n")
+            file.write(f"End Time: {add_checkout_table[i].end_time}\n")
+            file.write(f"Total Time Played: {add_checkout_table[i].time_played()}\n\n")
