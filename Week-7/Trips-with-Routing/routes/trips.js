3627b0fa1d651eec5f53ee805d95acf3aaa608a8
snmsaj <cehd@CEHDs-MacBook-Pro.local>
2021-03-18 19:09:16 -0400

    made updates page to trips-with-routing

--- Week-7/Trips-with-Routing/routes/trips.js
+++ Week-7/Trips-with-Routing/routes/trips.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {v4:uuidv4} = require('uuid')
 
-// /trips
+// Route is /trips
 router.get('/', (req, res) => {
     res.render('trips',{trips:tripList});
 })
@@ -30,5 +30,31 @@ router.post('/', (req, res) => {
     tripList = tripList.filter(trip => trip.id != id)
     res.redirect('/trips')
 })
+
+// Set the values to the updates page
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    const findId = tripList.find((trip) => trip.id == id);
+    res.render('update-trip', {trip:findId})
+})
+
+// Update a trip
+router.post('/:id', (req, res) => {
+    const title = req.body.title
+    const departTrip = req.body.departTrip
+    const returnTrip = req.body.returnTrip
+    const image = req.body.image
+    const id = req.body.id
+
+    let trip = tripList.find((trip) => trip.id == id)
+    
+    trip.title = title
+    trip.departTrip = departTrip
+    trip.returnTrip = returnTrip
+    trip.image = image
+
+    res.redirect('/trips')
+})
+
 
 module.exports = router
